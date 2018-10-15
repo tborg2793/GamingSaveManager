f8e6a276fc2a246d0511924e84c4f42134028d92
Thomas Borg <tbo@acunetix.com>
2018-10-15 16:54:36 +0200

    Added Logger Functionality Fixed loop for End of Watch and Process

--- GamingSaveManager/Program.cs
+++ GamingSaveManager/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 
@@ -7,60 +8,73 @@ namespace GamingSaveManager
     class Program
     {
         private static FileSystemWatcher watcher = new FileSystemWatcher();
-
+        private static Logger logger = new Logger();
+        private static bool gameProcess = false;
 
         static void Main(string[] args)
         {
-            Logger logger = new Logger();
-            logger.Debug("Hello");
-            Game game1 = new Game(1, "Middle-Earth", "C:\\Users\\tb\\Desktop\\test.txt", "G:\\MiddleEarth\\ME.exe");
-            //Console.Write(game1.gameName);
-            StartProcess();
-            Watch(game1);
-            //Console.ReadLine();
+
+            Game game1 = new Game(1, "Middle-Earth", "C:\\Users\\tb\\Desktop\\test.txt", "notepad.exe");
+
+            StartProcess(game1);
+            WatchSaveFile(game1);
+
+            while (gameProcess) ;
+
+            Console.ReadKey();
         }
 
-        public static void OnProcessExited(object sender, EventArgs e)
-        {
-            Console.WriteLine("Process has exited");
 
+        public static void StartProcess(Game game)
+        {
+            Process myProcess = Process.Start(game.exePath);
+            logger.Debug("[" + game.gameName + "] " + game.exePath + " process has started.");
+            myProcess.Exited += (sender, eventArgs) => OnProcessExited(game, eventArgs);
+
+
+            myProcess.EnableRaisingEvents = true;
+            gameProcess = true;
+        }
+
+
+        public static void OnProcessExited(Game game, EventArgs e)
+        {
+            logger.Debug("["+game.gameName+"] " + Path.GetFileName(game.exePath) + " process has exited");
+            gameProcess = false;
             watcher.Dispose();
         }
 
-        public static void StartProcess()
-        {
-            Process myProcess = Process.Start("notepad.exe");
-            myProcess.EnableRaisingEvents = true;
-            myProcess.Exited += OnProcessExited;
-        }
+
         
-        public static void Watch(Game game)
+        
+        public static void WatchSaveFile(Game game)
         {
             watcher.Path = Path.GetDirectoryName(game.savePath);
             watcher.NotifyFilter = NotifyFilters.FileName | NotifyFilters.Size;
             watcher.Filter = Path.GetFileName(game.savePath);
-            watcher.Changed += new FileSystemEventHandler(OnSaveUpdated);
+            watcher.Changed += (sender, eventArgs) => OnSaveUpdated(game, eventArgs);
+
             watcher.EnableRaisingEvents = true;
         }
 
-        private static void OnSaveUpdated(object source, FileSystemEventArgs e)
+        private static void OnSaveUpdated(Game game, FileSystemEventArgs e)
         {
             try
             {
                 watcher.EnableRaisingEvents = false;
-                Console.WriteLine(e.Name);
+                logger.Debug("["+game.gameName+"] " + Path.GetFileName(game.savePath) + " was updated.");
 
             }
             finally
             {
                 watcher.EnableRaisingEvents = true;
-
             }
 
         }
 
 
     }
+
 }
 
 
